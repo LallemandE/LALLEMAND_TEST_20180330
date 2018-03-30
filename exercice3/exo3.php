6e3b93590e7e1247e2e35c07f56a9de858e80e93
LallemandE <job@lallemand.lu>
2018-03-30 15:38:12 +0200

    cleaning of ex2

--- exercice3/exo3.php
+++ exercice3/exo3.php
@@ -1,64 +1,66 @@
 <?php
 /*
-In this exercise you will need to use PHP to handle a request to process an Ajax form.
-The form will add a new vehicle to the database and will contain the following fields:
-- Make
-- Model
-- Year
-- Colour
-The form will be processed in PHP. A success or error message will be displayed
-depending on the entry.
-Note: you do not need to use AJAX or SQL to do this, only PHP.
-  
+ * In this exercise you will need to use PHP to handle a request to process an Ajax form.
+ * The form will add a new vehicle to the database and will contain the following fields:
+ * - Make
+ * - Model
+ * - Year
+ * - Colour
+ * The form will be processed in PHP. A success or error message will be displayed
+ * depending on the entry.
+ * Note: you do not need to use AJAX or SQL to do this, only PHP.
+ *
  */
-
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
     
-    $message = '';
+    $errorMessage = '';
     
     $make = $_POST['Make'] ?? null;
     
-    // check that the "argument" has been given and, if so that it is a string else => I build an error message.
+    // $the argument should be a string else I start to build an error message.
     if (! $make || ! is_string($make)) {
-      $message .= 'Invalid Make Parameter';  
+        $errorMessage .= 'Invalid Make Parameter';
     }
     
     $model = $_POST['Model'] ?? null;
-
-    // check that the "argument" has been given and, if so that it is a string else => I build an error message.
-    // if there is already something in $message, I had a "separator" before adding the new content.
-    if (! $model || ! is_string($model)) {
-        if ($message) $message .= ' / ';
-        $message .= 'Invalid Make Parameter';
+    
+    // The model should be a string else I will add an error message.
+    // if there is already something in $errorMessage, I had a "separator" before adding the new content.
+    if (! is_string($model)) {
+        if ($errorMessage)
+            $errorMessage .= ' / ';
+        $errorMessage .= 'Invalid Make Parameter';
     }
     
     $year = $_POST['Year'] ?? null;
-
-    // check that the "argument" has been given and, if so that it is an integer else => I build an error message.
-    // if there is already something in $message, I had a "separator" before adding the new content.
-    if (! $year || ! is_int($year)) {
-        if ($message) $message .= ' / ';
-        $message .= 'Invalid Make Parameter';
+    
+    // The year should be an integer else I will add an error message.
+    // if there is already something in $errorMessage, I had a "separator" before adding the new content.
+    if (! is_int($year)) {
+        if ($errorMessage)
+            $errorMessage .= ' / ';
+        $errorMessage .= 'Invalid Make Parameter';
     }
     
     $colour = $_POST['Colour'] ?? null;
     
-    // check that the "argument" has been given and, if so that it is a string else => I build an error message.
-    // if there is already something in $message, I had a "separator" before adding the new content.
-    if (! $colour || ! is_string($colour)) {
-        if ($message) $message .= ' / ';
-        $message .= 'Invalid Colour Parameter';
+    // The colour should be a string else I will add an error message.
+    // if there is already something in $errorMessage, I had a "separator" before adding the new content.
+    if (! is_string($colour)) {
+        if ($errorMessage)
+            $errorMessage .= ' / ';
+        $errorMessage .= 'Invalid Colour Parameter';
     }
     
-    
-    if (! $message) {
+    // If the error message is empty
+    if (strlen($errorMessage) == 0) {
         // It is here that we could code SQL query to update the database.
         
         return "Request correctly executed !";
     }
     
-    return $message;
-    
+    return $errorMessage;
 }
 
+// if it is not a POST request.
 return "Invalid Request !";
